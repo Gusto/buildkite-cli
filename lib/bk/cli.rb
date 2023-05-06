212f672b7f4a0cdb1cd8ee86be2df137c487d694
Josh Nichols <josh.nichols@gusto.com>
2023-05-05 20:54:38 -0400

    quick attempt at artifacts

--- lib/bk/cli.rb
+++ lib/bk/cli.rb
@@ -50,6 +50,66 @@ module Bk
 
           @color_map
         end
+
+        private
+
+        def parse_slug_from_url(url)
+          # https://buildkite.com/my-org/my-pipeline/builds/1234 => my-org/my-pipeline/1234
+          url.delete_prefix("https://buildkite.com/").gsub("/builds", "")
+        end
+
+        def determine_slug(url_or_slug = nil)
+          if url_or_slug
+            if url_or_slug.start_with?("https:")
+              parse_slug_from_url(url_or_slug)
+            else
+              url_or_slug
+            end
+          else
+            output = `gh pr checks`
+            output.lines.each do |line|
+              if line =~ %r{https://buildkite.com/([^/]+)/([^/]+)/builds/(\d+)}
+                return "#{$1}/#{$2}/#{$3}"
+              end
+            end
+
+            nil
+          end
+        end
+
+        def query(graphql_query, **kwargs)
+          result = nil
+          spinner.run("Done") do |spinner|
+            result = Client.query(graphql_query, **kwargs)
+          end
+
+          result
+        end
+
+        def build_header(build, io: )
+          started_at = Time.parse(build.started_at)
+          finished_at = Time.parse(build.finished_at) if build.finished_at
+
+          build_color = color_map[build.state]
+
+          io.puts "#{build_color.call(vertical_pipe)}#{build.pipeline.slug} / #{build.branch}"
+          io.puts build_color.call(vertical_pipe)
+          io.puts "#{build_color.call(vertical_pipe)}    #{build.message}"
+          io.puts build_color.call(vertical_pipe)
+          io.puts "#{build_color.call(vertical_pipe)}Build ##{build.number}: #{build.state}"
+
+          if build.state == "RUNNING" || build.state == "FAILING"
+            duration = Time.now - started_at
+            minutes = (duration / 60).to_i
+            seconds = (duration % 60).to_i
+            io.puts "running for #{minutes}m #{seconds}s"
+          elsif finished_at
+            duration = finished_at - started_at
+            io.puts "#{build_color.call(vertical_pipe)}#{build.state.downcase.capitalize} in #{duration}s"
+          end
+
+          io.puts ""
+        end
       end
 
       class Annotations < Base
@@ -94,7 +154,6 @@ module Bk
 
         def call(args:, url_or_slug: nil)
           slug = determine_slug(url_or_slug)
-
           unless slug
             raise ArgumentError, "Unable to figure out slug to use"
           end
@@ -104,28 +163,7 @@ module Bk
           TTY::Pager.page do |page|
             build = result.data.build
 
-            started_at = Time.parse(build.started_at)
-            finished_at = Time.parse(build.finished_at) if build.finished_at
-
-            build_color = color_map[build.state]
-
-            page.puts "#{build_color.call(vertical_pipe)}#{build.pipeline.slug} / #{build.branch}"
-            page.puts build_color.call(vertical_pipe)
-            page.puts "#{build_color.call(vertical_pipe)}    #{build.message}"
-            page.puts build_color.call(vertical_pipe)
-            page.puts "#{build_color.call(vertical_pipe)}Build ##{build.number}: #{build.state}"
-
-            if build.state == "RUNNING" || build.state == "FAILING"
-              duration = Time.now - started_at
-              minutes = (duration / 60).to_i
-              seconds = (duration % 60).to_i
-              page.puts "running for #{minutes}m #{seconds}s"
-            elsif finished_at
-              duration = finished_at - started_at
-              page.puts "#{build_color.call(vertical_pipe)}#{build.state.downcase.capitalize} in #{duration}s"
-            end
-
-            page.puts ""
+            build_header(build, io: page)
 
             annotation_edges = build.annotations.edges
             annotations = annotation_edges.map { |edge| edge.node }
@@ -155,42 +193,87 @@ module Bk
           end
         end
 
-        private
+      end
 
-        def parse_slug_from_url(url)
-          # https://buildkite.com/my-org/my-pipeline/builds/1234 => my-org/my-pipeline/1234
-          url.delete_prefix("https://buildkite.com/").gsub("/builds", "")
-        end
+      class Artifacts < Base
+        desc "Show Artifacts for a Build"
+        argument :url_or_slug, type: :string, required: false, desc: "Build URL or Build slug"
 
-        def determine_slug(url_or_slug = nil)
-          if url_or_slug
-            if url_or_slug.start_with?("https:")
-              parse_slug_from_url(url_or_slug)
-            else
-              url_or_slug
-            end
-          else
-            output = `gh pr checks`
-            output.lines.each do |line|
-              if line =~ %r{https://buildkite.com/([^/]+)/([^/]+)/builds/(\d+)}
-                return "#{$1}/#{$2}/#{$3}"
-              end
-            end
+        BuildArtifactsQuery = Client.parse <<-GRAPHQL
+          query {
+            build(slug: "gusto/zenpayroll/759716") {
+              number
+              message
+              uuid
 
-            nil
+              branch
+              pipeline {
+                slug
+              }
+
+              url
+              pullRequest {
+                id
+              }
+              state
+              scheduledAt
+              startedAt
+              finishedAt
+              canceledAt
+
+              jobs(first:10) {
+                edges {
+                  node {
+                    __typename
+                    ... on JobTypeCommand {
+                      url
+                      uuid
+                      label
+                      artifacts(first: 10) {
+                        edges {
+                          node {
+                            state
+                            path
+                            downloadURL
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+
+        GRAPHQL
+
+        def call(args:, url_or_slug: nil)
+          slug = determine_slug(url_or_slug)
+          unless slug
+            raise ArgumentError, "Unable to figure out slug to use"
           end
-        end
 
-        def query(graphql_query, **kwargs)
-          result = nil
-          spinner.run("Done") do |spinner|
-            result = Client.query(BuildAnnotationsQuery, **kwargs)
+          result = query(BuildArtifactsQuery, variables: {slug: slug})
+          build = result.data.build
+
+          build_header(build, io: $stdout)
+
+          jobs = build.jobs.edges.map(&:node)
+
+          jobs.each do |job|
+            puts job.label
+
+            artifacts = job.artifacts.edges.map(&:node)
+
+            artifacts.each do |artifact|
+              puts "- #{artifact.path}"
+            end
           end
-
-          result
         end
       end
+
       register "annotations", Annotations
+      register "artifacts", Artifacts
     end
   end
 end
