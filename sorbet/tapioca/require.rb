501a6efa9d68fdb36c6391d0e3ff934fdb222d41
Alex Evanczuk <alex.evanczuk@gusto.com>
2023-06-08 14:05:03 -0400

    bin/tapioca require

--- sorbet/tapioca/require.rb
+++ sorbet/tapioca/require.rb
@@ -1,4 +1,16 @@
 # typed: true
 # frozen_string_literal: true
 
-# Add your extra requires here (`bin/tapioca require` can be used to boostrap this list)
+require "cgi"
+require "dry/cli"
+require "graphql/client"
+require "graphql/client/http"
+require "httparty"
+require "json"
+require "parallel"
+require "ruby-progressbar"
+require "tty-box"
+require "tty-markdown"
+require "tty-pager"
+require "tty-spinner"
+require "zeitwerk"
