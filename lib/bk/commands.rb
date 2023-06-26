3ec355c070bdcde73187c7feda958e7c54f62e49
Josh Nichols <josh.nichols@gusto.com>
2023-06-26 14:09:00 -0400

    Add bk logs

--- lib/bk/commands.rb
+++ lib/bk/commands.rb
@@ -14,15 +14,33 @@ module Bk
 
       private
 
-      def parse_slug_from_url(url)
+      def parse_build_slug_from_url(url)
         # https://buildkite.com/my-org/my-pipeline/builds/1234 => my-org/my-pipeline/1234
         url.delete_prefix("https://buildkite.com/").gsub("/builds", "")
       end
 
-      def determine_slug(url_or_slug = nil)
+      def parse_job_attributes_from_url(url)
+        # ie https://buildkite.com/gusto/zenpayroll/builds/791340#0188e817-b114-4896-95a0-83c02ac7d8e9
+        # => {
+        #   org: "gusto",
+        #   pipeline: "zenpayroll"
+        #   build_number: "791340"
+        #   job_id: "0188e817-b114-4896-95a0-83c02ac7d8e9"
+        # }
+        if url =~ %r{https://buildkite.com/([^/]+)/([^/]+)/builds/(\d+)#(.*)}
+          {
+            org: $1,
+            pipeline: $2,
+            build_number: $3,
+            job_id: $4
+          }
+        end
+      end
+
+      def determine_build_slug(url_or_slug = nil)
         if url_or_slug
           if url_or_slug.start_with?("https:")
-            parse_slug_from_url(url_or_slug)
+            parse_build_slug_from_url(url_or_slug)
           else
             url_or_slug
           end
@@ -38,6 +56,15 @@ module Bk
         end
       end
 
+      def determine_job_slug(url_or_slug)
+        # ie https://buildkite.com/gusto/zenpayroll/builds/791340#0188e817-b114-4896-95a0-83c02ac7d8e9
+        if url_or_slug.start_with?("https:")
+          parse_build_slug_from_url(url_or_slug)
+        else
+          url_or_slug
+        end
+      end
+
       def query(graphql_query, **kwargs)
         result = nil
         spinner.run("Done") do |spinner|
@@ -50,5 +77,6 @@ module Bk
 
     register "annotations", Annotations
     register "artifacts", Artifacts
+    register "logs", Logs
   end
 end
---
+++ lib/bk/commands/logs.rb
@@ -0,0 +1,27 @@
+module Bk
+  module Commands
+    class Logs < Base
+      desc "Show Artifacts for a Build"
+      argument :url_or_org, type: :string, required: false, desc: "Buildkite organization or Job URL"
+      argument :pipeline, type: :string, required: false, desc: "Buildkite pipeline"
+      argument :build_number, type: :string, required: false, desc: "Buildkite build number"
+      argument :job_id, type: :string, required: false, desc: "Buildkite job_id"
+
+      def call(args: {}, url_or_org: nil, pipeline: nil, build_number: nil, job_id: nil, **options)
+        attributes = if url_or_org.start_with?("https:")
+          parse_job_attributes_from_url(url_or_org)
+        else
+          {
+            org: url_or_org,
+            pipeline: pipeline,
+            build_number: build_number,
+            job_id: job_id
+          }
+        end
+
+        response = Bk.buildkit.job_log(attributes[:org], attributes[:pipeline], attributes[:build_number], attributes[:job_id])
+        puts response[:content]
+      end
+    end
+  end
+end
